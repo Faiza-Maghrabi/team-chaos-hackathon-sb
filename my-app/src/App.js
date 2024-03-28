739a4c1fc53b33783a94b8d8a851e42a81b999c0
Faiza-Maghrabi <faiza.maghrabi@gmail.com>
2024-03-28 14:15:59 +0000

    feat: hooks and button component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import {sendData} from "./server/Database"
+import { UploadButton } from "./ButtonTemp"
 
-const testData = {
-      Name: "Lisa",
-      Age: "35",
-      Number_of_Children: "4",
-      Visa_Status: "Refugee",
-      Occupation: "Bank",
-      Accomodation: "No",
-      Language_Level: "Intermediate",
-      Heath_Issues: "None",
-      first_lang: "Ukrainian",
-      has_child: "Yes",
-      job_uk: "No"
-}
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
@@ -24,7 +12,7 @@ function App() {
         <p>
           hii
         </p>
-        <button OnClick={sendData(testData.Name, testData.Age, testData.Number_of_Children, testData.Visa_Status, testData.Occupation, testData.Accomodation, testData.Language_Level, testData.Heath_Issues, testData.first_lang, testData.has_child, testData.job_uk)}>SendData</button>
+        <UploadButton />
       </header>
     </div>
   );
